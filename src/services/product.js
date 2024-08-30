05d53adad66b0eca6f0d090a2b581e84321087d8
elfazhr <120994386+elfazhr@users.noreply.github.com>
2024-08-30 13:57:52 +0700

    Delete src directory

--- src/services/product.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const getProducts = (callback) => {
-    axios.get("https://fakestoreapi.com/products").then((res) => {
-        callback(res.data)
-    }).catch((err) => {
-        console.log(err)
-    })
-}
